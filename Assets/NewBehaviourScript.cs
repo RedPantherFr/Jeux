702d0a92c9bfe29024b7edeacdc23aa011c9e408
RedPantherFr <51198902+RedPantherFr@users.noreply.github.com>
2019-05-30 14:45:30 +0200

    Jeux
    mise a jour

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float Speed=7f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    if(Input.GetKey(KeyCode.Q))
+        {
+            transform.Translate(-Vector3.right*Time.deltaTime*Speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(-Vector3.left * Time.deltaTime*Speed);
+        }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            transform.Translate(new Vector3(0,0,1) * Time.deltaTime * Speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, 0, -1) * Time.deltaTime * Speed);
+        }
+    }
+}
